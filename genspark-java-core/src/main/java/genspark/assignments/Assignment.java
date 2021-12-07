caef13abb5cc21974e3c1e2d5487851bd77e6cf9
Jacob Goodson <jacobegoodson@gmail.com>
2021-12-07 10:53:19 -0500

    cleanup

--- genspark-java-core/src/main/java/genspark/assignments/Assignment.java
+++ genspark-java-core/src/main/java/genspark/assignments/Assignment.java
@@ -6,14 +6,8 @@ import java.io.IOException;
 
 public interface Assignment {
     String ANSI_RESET = "\u001B[0m";
-    String ANSI_BLACK = "\u001B[30m";
     String ANSI_RED = "\u001B[31m";
-    String ANSI_GREEN = "\u001B[32m";
-    String ANSI_YELLOW = "\u001B[33m";
-    String ANSI_BLUE = "\u001B[34m";
     String ANSI_PURPLE = "\u001B[35m";
-    String ANSI_CYAN = "\u001B[36m";
-    String ANSI_WHITE = "\u001B[37m";
 
     default void test() throws IOException {
         if (
